172fdbfab90e8bea9bf3fc554bf6077dc7a5f754
NEMS Linux <nems@category5.tv>
2023-03-04 14:39:15 -0500

    Add TEMPerGold_V3.3 Support

--- debpack/usr/local/share/nems/nems-scripts/temper.py
+++ debpack/usr/local/share/nems/nems-scripts/temper.py
@@ -205,7 +205,7 @@ class USBRead(object):
       self._parse_bytes('internal temperature', 2, 256.0, bytes, info)
       return info
 
-    if info['firmware'][:15] in [ 'TEMPerGold_V3.1', 'TEMPerGold_V3.4' ]:
+    if info['firmware'][:15] in [ 'TEMPerGold_V3.1', 'TEMPerGold_V3.3', 'TEMPerGold_V3.4' ]:
       info['firmware'] = info['firmware'][:15]
       self._parse_bytes('internal temperature', 2, 100.0, bytes, info)
       return info
@@ -230,8 +230,8 @@ class USBRead(object):
       #Bytes 11-12 hold the external temp, divide by 100
       self._parse_bytes('external temperature', 10, 100.0, bytes, info, self.verbose)
       return info
-    if info['firmware'][:16] == 'TEMPerHUM_V3.9':
-      info['firmware'] = info['firmware'][:16]
+    if info['firmware'][:14] == 'TEMPerHUM_V3.9':
+      info['firmware'] = info['firmware'][:14]
       #Bytes 3-4 hold the device temp, divide by 100
       self._parse_bytes('internal temperature', 2, 100.0, bytes, info, self.verbose)
       #Bytes 11-12 hold the external temp, divide by 100
